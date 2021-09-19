4f19ef980263810e1775f94ba4aae498556cb27d
@dubanlam94 <dubanlam94@utp.edu.co>
2021-09-19 01:33:59 -0500

    Borrar html

---
+++ src/components/Mensaje.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="mensaje">
+        <p>Gracias por visitarnos! <br>Por favor valide si su reporte se encuentra dentro de los
+            registrados de la ultimas 72 horas,si es asi consulte el tiempo de respuesta aproximad
+            para su solucion y abstengase de generar uno nuevo con esto estara ayudando a ser mas
+            eficientes, en caso contrario por favor registrarse y generar el incidente.
+        </p>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
