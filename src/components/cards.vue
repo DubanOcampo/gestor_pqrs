e187e08ec12523d607af6d4267cfe8dcf878152d
@dubanlam94 <dubanlam94@utp.edu.co>
2021-09-19 22:31:59 -0500

    Nuevos componentes para Ãel mockup 1

---
+++ src/components/cards.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <v-card max-width="20%">
+      <v-card-text>
+        <div>Incidencia #X</div>
+        <p class="text-h4 text--primary">Tiempo Estimado: xxx</p>
+        <p>Barrio: xxxxxxxxxx</p>
+        <p>Dirrecion: xxxxxxxxxx</p>
+        <p>Fecha: xxxxxxxxxx</p>
+        <p>Hora: xxxxxxxxxx</p>
+        <p>descripcion: xxxxxxxxxx</p>
+      </v-card-text>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
