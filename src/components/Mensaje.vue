eea4787c93e5629c9b058e31adb638b98aeef045
unknown <mssyapg@gmail.com>
2021-09-24 17:18:29 -0500

    actualizacion vista1

--- src/components/Mensaje.vue
+++ src/components/Mensaje.vue
@@ -1,9 +1,10 @@
 <template>
   <div id="mensaje">
-        <p>Gracias por visitarnos! <br>Por favor valide si su reporte se encuentra dentro de los
-            registrados de la ultimas 72 horas,si es asi consulte el tiempo de respuesta aproximad
-            para su solucion y abstengase de generar uno nuevo con esto estara ayudando a ser mas
-            eficientes, en caso contrario por favor registrarse y generar el incidente.
+
+        <p> <br>Por favor valide si su reporte se encuentra dentro de los
+        registrados de la ultimas 72 horas,si es asi consulte el tiempo de respuesta aproximad
+        para su solucion y abstengase de generar uno nuevo con esto estara ayudando a ser mas
+        eficientes, en caso contrario por favor registrarse y generar el incidente.
         </p>
     </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,30 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Inicio/>
+    <h2> Gracias por Visitarnos!!</h2>
+    <h2> Incidentes Reportados en las ultimas 72 horas..</h2>
+    <Mensaje/>
+    <Cards/> 
+    <Cards/> 
+    
+    <Login/>
+    
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
+import Inicio from "@/components/Inicio.vue";
+import Login from "@/components/Login.vue";
+import Mensaje from "@/components/Mensaje.vue";
+import Cards from "@/components/Cards.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld,
+    HelloWorld,Inicio,Login,Mensaje,Cards,
   },
 };
 </script>
