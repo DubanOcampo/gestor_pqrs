d1782b79c449bd31c749f43e8ecef595abd7735b
@dubanlam94 <dubanlam94@utp.edu.co>
2021-09-27 19:57:56 -0500

    Tutoria con Emer

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,20 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Inicio</router-link> |
-      <router-link to="/about">Acerca</router-link>
-    </div>
-    <router-view/>
+    <navegacion></navegacion>
+    <div class="contenido">
+     <router-view />
+     </div>
   </div>
 </template>
+<script>
+import Navegacion from './components/Navegacion.vue'
+export default {
+  name: "app",
+  components:{
+    Navegacion
 
-<style lang="scss" scoped>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
   }
 }
+</script>
+<style>
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,11 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
     <Inicio/>
-    <h2> Gracias por Visitarnos!!</h2>
+    <h2 class="text-center"> Gracias por Visitarnos!!</h2>
     <h2> Incidentes Reportados en las ultimas 72 horas..</h2>
     <Mensaje/>
     <Cards/> 
     <Cards/> 
-    
-    <Login/>
-    
   </div>
 </template>
 
@@ -17,14 +13,17 @@
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
 import Inicio from "@/components/Inicio.vue";
-import Login from "@/components/Login.vue";
+
 import Mensaje from "@/components/Mensaje.vue";
 import Cards from "@/components/Cards.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld,Inicio,Login,Mensaje,Cards,
+    HelloWorld,Inicio,Mensaje,Cards,
   },
 };
 </script>
+<style lang="scss" scoped>
+
+</style>
